349b8d1b59a6d49d724da814e77abe88e9cf0373
Khuvi123 <b.tuguldur1213@gmail.com>
2025-06-07 08:47:33 +0800

    Update template or styling

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key-for-dev')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', 'innoknow.com']
+ALLOWED_HOSTS = ['.onrender.com', 'innoknow.com','127.0.0.1', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
